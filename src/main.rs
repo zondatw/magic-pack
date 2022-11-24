b4cff259b03042e790f437033f908c1680632f01
zonda yang <u226699@gmail.com>
2022-11-25 00:27:07 +0800

    Add file type checker

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use clap::{ArgGroup, Parser};
+use std::io::{self, Write};
+use std::process::Command;
 
-#[derive(Parser, Debug)]
+use clap::{ArgGroup, Parser, ValueEnum};
+
+#[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 #[command(group(
     ArgGroup::new("functional")
@@ -9,8 +12,12 @@ use clap::{ArgGroup, Parser};
 ))]
 struct Args {
     /// Compress flag
-    #[arg(short, long)]
+    #[arg(short, long, requires = "file_type")]
     compress: bool,
+
+    // file type
+    #[arg(short, value_enum)]
+    file_type: Option<FileType>,
 
     /// Decompress flag
     #[arg(short, long)]
@@ -25,18 +32,77 @@ struct Args {
     output: String,
 }
 
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum FileType {
+    Zip,
+    Tar,
+    Tarbz2,
+    Targz,
+}
+
+fn get_file_type(file_path: String) -> FileType {
+    let output = Command::new("file")
+        .arg(file_path)
+        .output()
+        .expect("file command failed");
+
+    if !output.status.success() {
+        panic!("file command failed");
+    }
+
+    let file_type = String::from_utf8(output.stdout).unwrap();
+    match file_type {
+        s if s.contains("Zip") => FileType::Zip,
+        s if s.contains("POSIX tar archive") => FileType::Tar,
+        s if s.contains("gzip") => FileType::Targz,
+        s if s.contains("bzip2") => FileType::Tarbz2,
+        _ => panic!("no supported"),
+    }
+}
+
 fn main() {
     let args = Args::parse();
 
+    println!("Input path: {:?}", args.input);
+    println!("Output path: {:?}", args.output);
+
     if args.compress {
         println!("Compress");
+
+        match args.file_type.unwrap() {
+            FileType::Zip => {
+                println!("Zip");
+            }
+            FileType::Tar => {
+                println!("Tar");
+            }
+            FileType::Tarbz2 => {
+                println!("Tarbz2");
+            }
+            FileType::Targz => {
+                println!("Targz");
+            }
+        }
     }
     if args.decompress {
         println!("Decompress");
-    }
+        let file_type = get_file_type(args.input);
 
-    println!("Input path: {:?}", args.input);
-    println!("Output path: {:?}", args.output);
+        match file_type {
+            FileType::Zip => {
+                println!("Zip");
+            }
+            FileType::Tar => {
+                println!("Tar");
+            }
+            FileType::Tarbz2 => {
+                println!("Tarbz2");
+            }
+            FileType::Targz => {
+                println!("Targz");
+            }
+        }
+    }
 
     // TODO: get file type
 }
