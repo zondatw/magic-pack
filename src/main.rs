be007e2afada7f7cb11e9abaa01db9181a116dbc
zonda yang <u226699@gmail.com>
2023-03-12 23:19:34 +0800

    Integrate function to modules which about file type

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,8 @@ mod cli;
 mod contents;
 mod modules;
 
-use std::process::Command;
-
 use crate::cli::Args;
 use crate::contents::enums;
-
-fn get_file_type(file_path: &std::string::String) -> enums::FileType {
-    let output = Command::new("file")
-        .arg(file_path)
-        .output()
-        .expect("file command failed");
-
-    if !output.status.success() {
-        panic!("file command failed");
-    }
-
-    let file_type = String::from_utf8(output.stdout).unwrap();
-    match file_type {
-        s if s.contains("Zip") => enums::FileType::Zip,
-        s if s.contains("POSIX tar archive") => enums::FileType::Tar,
-        s if s.contains("gzip") => enums::FileType::Targz,
-        s if s.contains("bzip2") => enums::FileType::Tarbz2,
-        _ => panic!("no supported"),
-    }
-}
 
 fn main() {
     let args = Args::new();
@@ -39,7 +17,7 @@ fn main() {
     }
     if args.decompress {
         println!("Decompress");
-        let file_type = get_file_type(&args.input);
+        let file_type = modules::get_file_type(&args.input);
         modules::decompress(file_type, &args.input, &args.output);
     }
 }
--- src/modules/mod.rs
+++ src/modules/mod.rs
@@ -1,6 +1,28 @@
 mod compression;
 
+use std::process::Command;
+
 use crate::enums;
+
+pub fn get_file_type(file_path: &std::string::String) -> enums::FileType {
+    let output = Command::new("file")
+        .arg(file_path)
+        .output()
+        .expect("file command failed");
+
+    if !output.status.success() {
+        panic!("file command failed");
+    }
+
+    let file_type = String::from_utf8(output.stdout).unwrap();
+    match file_type {
+        s if s.contains("Zip") => enums::FileType::Zip,
+        s if s.contains("POSIX tar archive") => enums::FileType::Tar,
+        s if s.contains("gzip") => enums::FileType::Targz,
+        s if s.contains("bzip2") => enums::FileType::Tarbz2,
+        _ => panic!("no supported"),
+    }
+}
 
 pub fn compress(
     file_type: enums::FileType,
