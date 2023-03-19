61bfc8515c65ecc79507f1e32ce32a24213b882f
zonda yang <u226699@gmail.com>
2023-03-19 12:34:56 +0800

    Seprate decompress function

--- src/main.rs
+++ src/main.rs
@@ -27,59 +27,62 @@ fn compress(
     }
 }
 
+fn decompress(src_path: &std::path::PathBuf, dst_path: &std::path::PathBuf) {
+    let filename = path::Path::new(src_path).file_stem().unwrap();
+    let temp_output = dst_path.join(filename);
+    println!("Decompress");
+    if dst_path != path::Path::new(".") {
+        fs::create_dir_all(&dst_path).expect("Create dir failed");
+    }
+    let mut decompress_output = temp_output;
+    let mut decompress_input = src_path.to_owned();
+    let _filename = path::Path::new(&decompress_output).file_name().unwrap();
+    let mg_filename = format!("{}{}", "mg_", _filename.to_str().unwrap());
+    decompress_output.set_file_name(mg_filename);
+
+    for index in 0..5 {
+        let file_type = match modules::get_file_type(
+            &decompress_input
+                .to_owned()
+                .into_os_string()
+                .into_string()
+                .unwrap(),
+        ) {
+            Ok(file_type) => file_type,
+            Err(e) if e.kind() == ErrorKind::Unsupported && index != 0 => {
+                let final_filename = decompress_input
+                    .file_name()
+                    .unwrap()
+                    .to_os_string()
+                    .into_string()
+                    .unwrap()
+                    .replace("mg_", "");
+                let mut decompress_final_output = path::PathBuf::from(&decompress_input);
+                decompress_final_output.set_file_name(final_filename);
+                fs::rename(decompress_input, decompress_final_output).expect("Rename failed");
+                break;
+            }
+            Err(e) => panic!("{}", e),
+        };
+        println!("Decompress output: {:?}", decompress_output.to_owned());
+        modules::decompress(file_type, &decompress_input, &decompress_output);
+        decompress_input = decompress_output;
+        let temp_filename = path::Path::new(&decompress_input).file_stem().unwrap();
+        decompress_output = path::PathBuf::from(&decompress_input);
+        decompress_output.set_file_name(temp_filename);
+    }
+}
+
 fn main() {
     let args = Args::new();
 
     println!("Input path: {:?}", args.input);
     println!("Output path: {:?}", args.output);
 
-    let filename = path::Path::new(&args.input).file_stem().unwrap();
-    let temp_output = args.output.join(filename);
-
     if args.compress {
         compress(&args.file_type.unwrap(), &args.input, &args.output);
     }
     if args.decompress {
-        println!("Decompress");
-        if args.output != path::Path::new(".") {
-            fs::create_dir_all(&args.output).expect("Create dir failed");
-        }
-        let mut decompress_output = temp_output;
-        let mut decompress_input = args.input;
-        let _filename = path::Path::new(&decompress_output).file_name().unwrap();
-        let mg_filename = format!("{}{}", "mg_", _filename.to_str().unwrap());
-        decompress_output.set_file_name(mg_filename);
-
-        for index in 0..5 {
-            let file_type = match modules::get_file_type(
-                &decompress_input
-                    .to_owned()
-                    .into_os_string()
-                    .into_string()
-                    .unwrap(),
-            ) {
-                Ok(file_type) => file_type,
-                Err(e) if e.kind() == ErrorKind::Unsupported && index != 0 => {
-                    let final_filename = decompress_input
-                        .file_name()
-                        .unwrap()
-                        .to_os_string()
-                        .into_string()
-                        .unwrap()
-                        .replace("mg_", "");
-                    let mut decompress_final_output = path::PathBuf::from(&decompress_input);
-                    decompress_final_output.set_file_name(final_filename);
-                    fs::rename(decompress_input, decompress_final_output).expect("Rename failed");
-                    break;
-                }
-                Err(e) => panic!("{}", e),
-            };
-            println!("Decompress output: {:?}", decompress_output.to_owned());
-            modules::decompress(file_type, &decompress_input, &decompress_output);
-            decompress_input = decompress_output;
-            let temp_filename = path::Path::new(&decompress_input).file_stem().unwrap();
-            decompress_output = path::PathBuf::from(&decompress_input);
-            decompress_output.set_file_name(temp_filename);
-        }
+        decompress(&args.input, &args.output);
     }
 }
