92dc09e3bd08c9a79fe1626f81e4a6d3ad367c85
zonda yang <u226699@gmail.com>
2026-02-23 21:54:05 +0800

    Add example msg for cli's help

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -4,7 +4,29 @@ use std::path;
 use magic_pack::contents::enums;
 
 #[derive(Parser)]
-#[command(author, version, about, long_about = None)]
+#[command(
+    author,
+    version,
+    about,
+    long_about = None,
+    after_help =
+"Examples:
+  magic-pack -c -f zip -o temp/temp.zip src
+  magic-pack -d -o temp/. temp/temp.zip
+  magic-pack -c -f gz -o temp/file.txt.gz temp/file.txt
+  magic-pack -d -o temp/. temp/file.txt.gz
+  magic-pack -c -f bz2 -o temp/file.txt.bz2 temp/file.txt
+  magic-pack -d -o temp/. temp/file.txt.bz2
+  magic-pack -c -f tar -o temp/temp.tar src
+  magic-pack -d -o temp/. temp/temp.tar
+  magic-pack -c -f tarbz2 -o temp/temp.tar.bz2 src
+  magic-pack -d -o temp/. temp/temp.tar.bz2
+  magic-pack -c -f targz -o temp/temp.tar.gz src
+  magic-pack -d -o temp/. temp/temp.tar.gz
+  magic-pack -d -l 3 -o temp/. temp/archive.tar.gz
+  magic-pack -d temp/temp.zip
+"
+)]
 #[command(group(
     ArgGroup::new("functional")
         .required(true)
